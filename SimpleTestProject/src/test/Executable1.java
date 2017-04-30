76b99ca6ba204f1d3139e95f716286aada957bd0
skryzhya <skrudge@yahoo.com>
2017-04-30 12:50:20 -0400

    Initial commit "Simple Test Project"

---
+++ SimpleTestProject/src/test/Executable1.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Executable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is executable 1!");
+	}
+
+}
---
+++ SimpleTestProject/src/test/Executable2.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Executable2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is executable 2!");
+	}
+
+}
