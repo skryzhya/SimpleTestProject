8ffb8649d8075af5be78d2baeab939110da808cb
skryzhya <skrudge@yahoo.com>
2017-04-30 13:10:00 -0400

    Minor change in print statement.

--- SimpleTestProject/src/test/Executable1.java
+++ SimpleTestProject/src/test/Executable1.java
@@ -4,7 +4,7 @@ public class Executable1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is executable 1!");
+		System.out.println("This is executable 1!!!");
 	}
 
 }
