a83702c33536f9535d09b96d024207ffb369bae6
skryzhya <skrudge@yahoo.com>
2023-09-30 01:06:19 -0400

    Made a simple text change in a print statement.

--- SimpleTestProject/src/test/Executable2.java
+++ SimpleTestProject/src/test/Executable2.java
@@ -4,7 +4,7 @@ public class Executable2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is executable 2!");
+		System.out.println("This is executable 2! New text!");
 	}
 
 }
