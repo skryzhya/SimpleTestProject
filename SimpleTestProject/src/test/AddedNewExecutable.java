b8b34cc1f80d2047326addfa59448836e0bfc687
skryzhya <skrudge@yahoo.com>
2023-10-03 16:31:34 -0400

    This is merge conflict #2

--- SimpleTestProject/src/test/AddedNewExecutable.java
+++ SimpleTestProject/src/test/AddedNewExecutable.java
@@ -5,7 +5,7 @@ public class Executable1 {
 	public static void main(String[] args) {
 		System.out.println("Added a brand new file to version control: GIT !");
 		System.out.println("Updated and committed chages to the new file on Github!");
-		System.out.println("Creating a merge conflict 1");
+		System.out.println("Yes, this is a merge conflict 2");
 	}
 
 }
