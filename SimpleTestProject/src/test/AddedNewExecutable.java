5126cfcbc304e3635b9562f274a1f0e72484de02
skryzhya <skrudge@yahoo.com>
2023-10-03 14:31:28 -0400

    New commit 1 - will be deleted.

--- SimpleTestProject/src/test/AddedNewExecutable.java
+++ SimpleTestProject/src/test/AddedNewExecutable.java
@@ -5,6 +5,7 @@ public class Executable1 {
 	public static void main(String[] args) {
 		System.out.println("Added a brand new file to version control: GIT !");
 		System.out.println("Updated and committed chages to the new file on Github!");
+		System.out.println("New commit 1");
 	}
 
 }
